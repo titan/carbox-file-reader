3a52a86ab97390986ef653042c1a707cc14744fb
Titan <howay.tan@gmail.com>
2017-09-02 15:51:40 +0800

    Remove useless files

--- src/main/kotlin/com/fengchaohuzhu/reader/Reader.kt
+++ src/main/kotlin/com/fengchaohuzhu/reader/Reader.kt
@@ -10,35 +10,35 @@ import java.io.Reader;
 public data class Index(val filename: String? = null, val checkSum: String, val size: Int, var url: String? = null)
 fun notNull(str: String) =str !=""
 
-public fun main(args: Array<String>){
-	println("Reader")
-	val filename = """/home/luofei/Works/text.txt"""
-	val res = getIndexByPath(filename)
-	println(res)
+public fun main(args: Array<String>) {
+  println("Reader")
+  val filename = """/home/luofei/Works/text.txt"""
+  val res = getIndexByPath(filename)
+  println(res)
 }
 
 
 public fun getIndexByPath(path: String): MutableList<Index> {
-	val file = File(path)
-	val contents = file.readText()
-	val list: MutableList<Index> = getIndexByContent(contents)
-	return list
+  val file = File(path)
+  val contents = file.readText()
+  val list: MutableList<Index> = getIndexByContent(contents)
+  return list
 }
 
-public fun getIndexByContent(content: String): MutableList<Index>{
-	val	strarr = content.split("\n")
-	val new_strarr = strarr.filter(::notNull)
-	var list = mutableListOf<Index>()
-	for(i in 0..new_strarr.size-1) {
-		if(i == 0) {
-			var strarray: List<String> = new_strarr.get(i).split("|")
-			val index = Index(filename = strarray.get(0), checkSum = strarray.get(1), size = (strarray.get(2)).toInt())
-			list.add(index)
-		}else {
-			var strarray: List<String> = new_strarr.get(i).split("|")
-			val index = Index(url = strarray.get(0), checkSum = strarray.get(1), size = (strarray.get(2)).toInt())
-			list.add(index)
-		}
-	}
-	return list
+public fun getIndexByContent(content: String): MutableList<Index> {
+  val  strarr = content.split("\n")
+  val new_strarr = strarr.filter(::notNull)
+  var list = mutableListOf<Index>()
+  for(i in 0..new_strarr.size-1) {
+    if(i == 0) {
+      var strarray: List<String> = new_strarr.get(i).split("|")
+      val index = Index(filename = strarray.get(0), checkSum = strarray.get(1), size = (strarray.get(2)).toInt())
+      list.add(index)
+    }else {
+      var strarray: List<String> = new_strarr.get(i).split("|")
+      val index = Index(url = strarray.get(0), checkSum = strarray.get(1), size = (strarray.get(2)).toInt())
+      list.add(index)
+    }
+  }
+  return list
 }
